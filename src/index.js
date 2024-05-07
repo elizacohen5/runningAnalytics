c33551f98035eebe144c2b2cdde30109f644eedb
Eliza Cohen <elizacohen5@gmail.com>
2024-05-07 09:12:19 -0400

    reformatting

--- src/index.js
+++ src/index.js
@@ -14,54 +14,56 @@ function createCell(column, content, str, element) {
     element.append(column);
 }
 
+function createRunner(athlete) {
+    const tableRow = document.createElement("tr");
+    tableRow.id = athlete.id;
+    tableRow.className = "runner-data"
+    document.querySelector("table").append(tableRow);
+    const pace = Math.round((athlete.time / athlete.distance) * 10) / 10;
+
+    const col1 = document.createElement("td");
+    const col2 = document.createElement("td");
+    const col3 = document.createElement("td");
+    const col4 = document.createElement("td");
+
+    createCell(col1, athlete.name, "", tableRow)
+    createCell(col2, athlete.distance, " Miles", tableRow)
+    createCell(col3, athlete.time, " Minutes", tableRow)
+    createCell(col4, pace, " min/mi", tableRow)
+
+    const buttonCol = document.createElement("td");
+    const button = document.createElement("button")
+    button.className = "button"
+    button.id = `button${athlete.id}`;
+    console.log(button.id);
+    button.textContent = "x"
+    buttonCol.appendChild(button)
+    tableRow.append(buttonCol);
+
+    document.getElementById(`${tableRow.id}`).addEventListener("mouseenter", (event) => {
+        const targetRow = event.target;
+        targetRow.style["background-color"] = "yellow"; 
+    })
+
+    document.getElementById(`${tableRow.id}`).addEventListener("mouseleave", (event) => {
+        const targetRow = event.target;
+        targetRow.style["background-color"] = "white"; 
+    })
+
+    document.getElementById(button.id).addEventListener("click", () => {
+        deleteRunner(athlete.id);
+        document.getElementById(`${tableRow.id}`).remove();
+        console.log(`The deleted table id is: ${tableRow.id}`);
+    })
+}
+
 function displayRunners() {
         fetch("http://localhost:3000/runners")
         .then(response => response.json())
         // Need to sort an array of objects based on one value in the array
         .then(runners => {
-            const unSortedRunners = runners.map((runner, index) => {
-                const tableRow = document.createElement("tr");
-                tableRow.id = runner.id;
-                console.log(tableRow.id)
-
-                tableRow.className = "runner-data"
-                document.querySelector("table").append(tableRow);
-                const pace = Math.round((runner.time / runner.distance) * 10) / 10;
-
-                const col1 = document.createElement("td");
-                const col2 = document.createElement("td");
-                const col3 = document.createElement("td");
-                const col4 = document.createElement("td");
-                
-                createCell(col1, runner.name, "", tableRow)
-                createCell(col2, runner.distance, " Miles", tableRow)
-                createCell(col3, runner.time, " Minutes", tableRow)
-                createCell(col4, pace, " min/mi", tableRow)
-
-                const buttonCol = document.createElement("td");
-                const button = document.createElement("button")
-                button.className = "button"
-                button.id = `button${index}`
-                console.log(button.id)
-                button.textContent = "x"
-                buttonCol.appendChild(button)
-                tableRow.append(buttonCol);
-
-                document.getElementById(`${tableRow.id}`).addEventListener("mouseenter", (event) => {
-                    const targetRow = event.target;
-                    targetRow.style["background-color"] = "yellow"; 
-                })
-
-                document.getElementById(`${tableRow.id}`).addEventListener("mouseleave", (event) => {
-                    const targetRow = event.target;
-                    targetRow.style["background-color"] = "white"; 
-                })
-
-                document.getElementById(button.id).addEventListener("click", () => {
-                    deleteRunner(runner.id);
-                    document.getElementById(`${tableRow.id}`).remove();
-                    console.log(`The deleted table id is: ${tableRow.id}`);
-                })
+            const unSortedRunners = runners.map((runner) => {
+                createRunner(runner);
             })
         })
 }
@@ -84,46 +86,7 @@ function addSubmitListener() {
         })
         .then(response => response.json())
         .then(runner => {
-            const tableRow = document.createElement("tr");
-            tableRow.id = runner.id;
-            tableRow.className = "runner-data"
-            document.querySelector("table").append(tableRow);
-            const pace = Math.round((runner.time / runner.distance) * 10) / 10;
-
-            const col1 = document.createElement("td");
-            const col2 = document.createElement("td");
-            const col3 = document.createElement("td");
-            const col4 = document.createElement("td");
-
-            createCell(col1, runner.name, "", tableRow)
-            createCell(col2, runner.distance, " Miles", tableRow)
-            createCell(col3, runner.time, " Minutes", tableRow)
-            createCell(col4, pace, " min/mi", tableRow)
-
-            const buttonCol = document.createElement("td");
-            const button = document.createElement("button")
-            button.className = "button"
-            button.id = `button${runner.id}`;
-            console.log(button.id);
-            button.textContent = "x"
-            buttonCol.appendChild(button)
-            tableRow.append(buttonCol);
-
-            document.getElementById(`${tableRow.id}`).addEventListener("mouseenter", (event) => {
-                const targetRow = event.target;
-                targetRow.style["background-color"] = "yellow"; 
-            })
-
-            document.getElementById(`${tableRow.id}`).addEventListener("mouseleave", (event) => {
-                const targetRow = event.target;
-                targetRow.style["background-color"] = "white"; 
-            })
-
-            document.getElementById(button.id).addEventListener("click", () => {
-                deleteRunner(runner.id);
-                document.getElementById(`${tableRow.id}`).remove();
-                console.log(`The deleted table id is: ${tableRow.id}`);
-            })
+            createRunner(runner);
             document.getElementById("runner-info").reset();
         })
     })
